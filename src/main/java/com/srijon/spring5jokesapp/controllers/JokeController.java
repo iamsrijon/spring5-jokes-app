63feae20a5cb9b4fdba34d36de2be09139cbf74a
Shuvradeb Barman Srijon <rusrijon@yahoo.com>
2022-09-27 03:40:33 -0500

    Create Jokes Controller
    closes #4

---
+++ src/main/java/com/srijon/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package com.srijon.spring5jokesapp.controllers;
+
+import com.srijon.spring5jokesapp.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
+        return "index";
+    }
+}
